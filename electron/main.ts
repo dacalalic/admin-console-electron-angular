9bd682893842f14cfa66fce8acca5f0c350d8c90
Danilo Lalic <dlalic98@gmail.com>
2026-03-20 14:49:21 +0100

    Setup Angular and Electron

---
+++ electron/main.ts
@@ -0,0 +1,36 @@
+import { app, BrowserWindow } from 'electron';
+import path from 'node:path';
+
+function createWindow(): void {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 900,
+    minWidth: 1000,
+    minHeight: 700,
+    autoHideMenuBar: true,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      nodeIntegration: false,
+      sandbox: true,
+    },
+  });
+
+  void win.loadURL('http://localhost:4200');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
